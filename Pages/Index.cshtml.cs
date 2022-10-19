a66a4f18d1630847281137a351cb0459753eddd8
Hans Naert <hans.naert@vives.be>
2022-10-19 21:06:53 +0200

    version that drives output 18 after checking out - libman must be run with "libman restore"

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Device.Gpio;
+
+namespace aspnetcoreapp.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    private GpioController controller;
+
+    public IndexModel(GpioController controller, ILogger<IndexModel> logger)
+    {
+        this.controller=controller;
+        _logger = logger;
+    }
+
+    [BindProperty]
+    public bool Output18 { get; set; }
+
+    public void OnGet()
+    {
+
+    }
+
+    public void OnPost()
+    {
+        //using(var controller = new GpioController())
+        {
+           // controller.OpenPin(18,PinMode.Output);
+            controller.Write(18,Output18?PinValue.High:PinValue.Low);
+            Console.WriteLine($"Output 1 is {Output18}");
+        }
+    }
+
+}
